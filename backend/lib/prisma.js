4c2de6a594dd738d6790bfe27651d5df75065104
Bhavesh-ahuja <151507950+Bhavesh-ahuja@users.noreply.github.com>
2025-12-12 15:47:15 +0530

    Update prisma.js

--- backend/lib/prisma.js
+++ backend/lib/prisma.js
@@ -1,5 +1,13 @@
+// backend/lib/prisma.js
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      // tell Prisma client the DB URL directly from the environment
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 export default prisma;
